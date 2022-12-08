2b9969a4460e322d7f23b15ff16cc39ef4f99ee6
zsachin.modi <sachin.mynt@perennialsys.com>
2022-12-08 16:43:08 +0530

    Made changes for testing the gradle.properties file shows on the github or not.

--- app/src/main/java/com/example/sampleci_cd/MainActivity.kt
+++ app/src/main/java/com/example/sampleci_cd/MainActivity.kt
@@ -2,12 +2,13 @@ package com.example.sampleci_cd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private val tvText by lazy { findViewById<TextView>(R.id.tv_text) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
+        tvText.text = BuildConfig.BASE_URL
     }
 }
